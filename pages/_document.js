5c7766d6fd99bfa3c4e723b6f7ac72f3916e95e7
bmccann1024 <91769196+bmccann1024@users.noreply.github.com>
2026-03-26 18:10:28 -0400

    Fix favicon link in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="favicon.jpg.png" />
+        <link rel="icon" href="favicon" />
       </Head>
       <body>
         <Main />
